59c214c0c86906cf8c76e603588747fa4ce066c3
chris010970 <c.r.williams0109@gmail.com>
2021-08-18 11:25:58 +0100

    Updated client / notebook with pipeline configuration / data download fucntionality

--- src/client.py
+++ src/client.py
@@ -1,13 +1,17 @@
 import os
+import wget
 import uuid
 import json
 import time
+import shutil
 import pycurl
 import certifi
+import tempfile
 import pandas as pd
 
 from io import BytesIO
 from io import StringIO
+from datetime import datetime
 
 class EcClient:
 
@@ -27,7 +31,7 @@ class EcClient:
 
         # load template json objects from file            
         self._templates = dict()
-        for key in [ 'search', 'pipeline' ]:
+        for key in [ 'search', 'pipeline', 'pipeline-search' ]:
 
             with open( os.path.join( path, f'{key}.json' ), ) as f:
                 self._templates[ key ] = json.load( f )
@@ -128,7 +132,7 @@ class EcClient:
             while True:
 
                 # get search result
-                status, result = client.getSearch( search_id )
+                status, result = self.getSearch( search_id )
                 if status != 202:
                     break
 
@@ -158,6 +162,29 @@ class EcClient:
         return self.sendRequest( self._uris[ 'base' ] + f'/pipelines/{pipeline_id}' )
 
 
+    def getPipelineIdFromName( self, name ):
+
+        """
+        get pipeline associated with id
+        """
+
+        pipeline_id = None
+
+        # run get request
+        status, result = self.sendRequest( self._uris[ 'base' ] + f'/pipelines' )
+        if status == 200:
+
+            # parse into dataframe
+            df = pd.DataFrame( result[ 'data'] )
+            df = df[ df['name'] == name ]
+
+            # get id from row
+            if len( df == 1 ):
+                pipeline_id = df[ 'id' ].iloc[ 0 ]
+        
+        return pipeline_id
+
+
     def deletePipeline( self, pipeline_id ):
 
         """
@@ -168,7 +195,7 @@ class EcClient:
         return self.sendRequest( self._uris[ 'base' ] + f'/pipelines/{pipeline_id}', action='DELETE' )
 
 
-    def createPipeline( self, aoi, window, **kwargs ):
+    def createPipelineFromSearch( self, search_id, search_results, **kwargs ):
 
         """
         create pipeline
@@ -181,19 +208,17 @@ class EcClient:
             """
 
             # configure payload
-            payload =  self._templates[ 'pipeline' ]
-            payload[ 'aoi' ] = aoi
+            payload = self._templates[ 'pipeline-search' ]
 
             # get time range
-            payload[ 'start_date' ] = window[ 'start_date' ]
-            payload[ 'end_date' ] = window[ 'end_date' ]
+            payload[ 'search_id' ] = search_id
+            payload[ 'search_results' ] = search_results
 
             # assign max cost
             payload[ 'max_cost' ] = self._max_cost
 
             # for each template field
-            keys = list( payload.keys() ) + [ 'search_id', 'search_results' ]
-            for key in keys:
+            for key in list( payload.keys() ):
 
                 # replace tenplate values with kwargs
                 value = kwargs.get( key )
@@ -222,6 +247,110 @@ class EcClient:
 
         # return status code and response
         return request.getinfo(pycurl.RESPONSE_CODE ), json.loads( response.getvalue() )
+
+
+    def getIntervalResults( self, pipeline_id ):
+
+        """
+        get interval results
+        """
+
+        # run get request
+        return self.sendRequest( self._uris[ 'base' ] + f'/pipelines/{pipeline_id}/interval_results' )
+
+
+    def getImages( self, results, out_path ):
+
+        """
+        get request
+        """
+
+        def getDateTimePath( metafile ):
+
+            """
+            get datetime path
+            """
+
+            with open( metafile ) as f:
+                data = json.load( f )
+                dt = datetime.strptime( data[ 'ProductInfo' ][ 'PRODUCT_SCENE_RASTER_START_TIME' ], '%d-%b-%Y %H:%M:%S.%f')
+                
+            return dt.strftime( '%Y%m%d_%H%M%S' )
+
+
+        images = []
+
+        # convert to dataframe if required
+        if not isinstance( results, pd.DataFrame ):
+            results = pd.DataFrame( results )
+
+        for row in results.itertuples():
+
+            # download metadata file
+            with tempfile.TemporaryDirectory() as tmpdir:
+                print ( '... downloading {url}'.format( url=row.metadata_url ) )
+                metafile = wget.download( row.metadata_url, out=tmpdir )
+
+                # determine datetime path from matadata
+                path = os.path.join( out_path, getDateTimePath( metafile ) )
+                if not os.path.exists( path ):
+                    os.makedirs( path )
+
+                # move metadata file to out_path if not exists
+                pathname = os.path.join( path, os.path.basename( row.metadata_url ) )
+                if not os.path.exists( pathname ):
+                    shutil.move( metafile, path )
+
+            # download scientific dataset to out_path datetime folder
+            pathname = os.path.join( path, os.path.basename( row.analytics_url ) )
+            if not os.path.exists( pathname ):
+                print ( '... downloading {url}'.format( url=row.analytics_url ) )
+                images.append( wget.download( row.analytics_url, out=path ) )
+
+        return images
+
+
+    def getOutputs( self ):
+
+        """
+        get request
+        """
+
+        # run get request
+        return self.sendRequest( self._uris[ 'base' ] + f'/outputs' )
+
+
+    def getOutputIdFromName( self, name ):
+
+        """
+        get request
+        """
+
+        output_id = None
+
+        # run get request
+        status, result = self.sendRequest( self._uris[ 'base' ] + f'/outputs' )
+        if status == 200:
+
+            # parse into dataframe
+            df = pd.DataFrame( result[ 'data'] )
+            df = df[ df['name'] == name ]
+
+            # get id from row
+            if len( df == 1 ):
+                output_id = df[ 'id' ].iloc[ 0 ]
+        
+        return output_id
+            
+
+    def getOutput( self, output_id ):
+
+        """
+        get request
+        """
+
+        # run get request
+        return self.sendRequest( self._uris[ 'base' ] + f'/outputs/{output_id}' )
 
 
     def initRequest( self, uri ):
